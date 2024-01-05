29ba71bb6ed0e5c9f99f07e5a4d7f77db9b1ec7a
Saurabh Sharma <saurabh@courial.com>
2024-01-05 18:09:45 +0530

    Initial Commit

---
+++ CloudAngles/CloudAnglesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudAnglesApp.swift
+//  CloudAngles
+//
+//  Created by Saurabh Sharma on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CloudAnglesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloudAngles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CloudAngles
+//
+//  Created by Saurabh Sharma on 05/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
